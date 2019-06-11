982a8bd861e977ab25e4a1c777faca625782d054
@mauroef <cypress90@gmail.com>
2019-06-11 00:50:53 -0300

    initial commit

---
+++ public/src/index.js
@@ -0,0 +1,10 @@
+import '@scss/freelancer.scss';
+import 'jquery';
+// bootstrap core
+import '../src/vendor/bootstrap/js/bootstrap.bundle.min.js';
+import '../src/vendor/jquery-easing/jquery.easing.min.js';
+// contact gorm
+import '../src/js/jqBootstrapValidation.js';
+import '../src/js/contact_me.js';
+// custom scripts for this template
+import '../src/js/freelancer.js';
